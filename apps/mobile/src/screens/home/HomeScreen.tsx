cdc3633e93fea13148c3459ec8b2ef8171136cb3
JasonMayo23587572 <u23587572@tuks.co.za>
2025-05-24 18:42:58 +0200

    File Structure Creation & HomeScreen

---
+++ apps/mobile/src/screens/home/HomeScreen.tsx
@@ -0,0 +1,23 @@
+// src/screens/HomeScreen.tsx
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Gym Manager Mobile</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});

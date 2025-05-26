2b077ec53d186140582cf7ae286e7fb6b4b3fedf
Amadeus Fidos <u22526162@tuks.co.za>
2025-05-26 13:12:17 +0200

    styles updated, logout button

--- apps/web/src/app/dashboard/layout.tsx
+++ apps/web/src/app/dashboard/layout.tsx
@@ -1,12 +1,17 @@
+'use client'
 import Link from "next/link"
 import Image from "next/image";
 import "./dashboard-layout.css";
-
+import { useRouter } from "next/navigation"; 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+    const router = useRouter();
+    const handleLogout = async () => {
+        router.push("/")
+    };
     return(
         <div className="dashboard-layout">
             <nav className="sidebar">
@@ -20,6 +25,9 @@ export default function DashboardLayout({
                     <li><Link href="/dashboard/reports">Packages</Link></li>
                     <li><Link href="/dashboard/reports">Reports</Link></li>
                 </ul>
+                <div className="logout-container">
+                    <Link href={'#'} onClick={handleLogout} className="logout">Logout</Link>
+                </div>
             </nav>
             <main>{children}</main>
         </div>

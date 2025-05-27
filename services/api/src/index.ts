769c7459e4a14a3c3c8c7caf92d33de8cb83f330
Amadeus Fidos <u22526162@tuks.co.za>
2025-05-28 01:10:21 +0200

    api port update

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -9,7 +9,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(cors({
   origin: 'http://localhost:3000', 

053c7c6d3a7aad732fa3b41b3945f04951c54869
Denis Woolley <u23528860@tuks.co.za>
2025-06-23 19:58:01 +0200

    "Live" class endpoint updated

--- services/api/src/routes/ongoingClass.ts
+++ services/api/src/routes/ongoingClass.ts
@@ -1,12 +1,12 @@
 // === services/api/src/routes/auth.ts ===
 import express from 'express';
 import { getLeaderboard } from '../controllers/ongoingClassController';
-import { getCurrentClass } from '../controllers/ongoingClassController';
+import { getLiveClass } from '../controllers/ongoingClassController';
 import { isAuthenticated } from '../middleware/auth';
 
 const router = express.Router();
 
 router.get('/leaderboard', isAuthenticated, getLeaderboard);
-router.get('/getCurrentClass', isAuthenticated, getCurrentClass);
+router.get('/live/class', isAuthenticated, getLiveClass);
 
 export default router;

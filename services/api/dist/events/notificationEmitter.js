6bdc378660e4030b634cbe75c629daecd2b7a0fc
JasonMayo23587572 <141422819+JasonMayo23587572@users.noreply.github.com>
2025-09-11 20:50:45 +0200

    Delete services/api/dist directory
    As per team lead request

--- services/api/dist/events/notificationEmitter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notificationEmitter = void 0;
-const events_1 = require("events");
-class NotificationEmitter extends events_1.EventEmitter {
-}
-exports.notificationEmitter = new NotificationEmitter();

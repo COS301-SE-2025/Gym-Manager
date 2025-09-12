37ce689b33bcf805f9a9c6f28c980553a8a0bc4a
Vansh-Sood <u23534402@tuks.co.za>
2025-09-12 13:42:47 +0200

    temp removed index.ts

--- services/api/src/index.ts
+++
@@ -1,7 +0,0 @@
-// import App from './app';
-
-// // Create app instance for testing
-// const appInstance = new App();
-// const app = appInstance.getApp();
-// export { app };
-// export default app;
